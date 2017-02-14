74ba90db5943399f4d84355db66bf49fd2d55e26
Otto <yuh198@ucsd.edu>
2017-02-14 12:42:43 -0800

    test code, create 4 groups

--- app/src/main/java/teamhardcoder/y_fi/MainActivity.java
+++ app/src/main/java/teamhardcoder/y_fi/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
+import teamhardcoder.y_fi.database.data.Group;
 import teamhardcoder.y_fi.database.manager.GroupManager;
 import teamhardcoder.y_fi.database.manager.ManagerFactory;
 
@@ -17,6 +18,10 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -39,11 +44,52 @@ public class MainActivity extends AppCompatActivity {
                 DynamoDBMapper mapper = DatabaseHelper.getDBMapper(getApplicationContext());
 
 
-                Expense temp = mapper.load(Expense.class, "95b075ea-86ac-442c-abfc-878eeb8dd23e");
-                System.out.println("Testing!!  " + temp.getCategoryName());
+                Group tmp = mapper.load(Group.class, "95b075ea-86ac-442c-abfc-878eeb8dd23e");
+                System.out.println("Testing!! ");
                 System.out.println("Hi Everyone!");
 
+
+                Set<String> tgsa = new HashSet<String>();
+                tgsa.add("otto");
+                tgsa.add("chris");
+                tgsa.add("aaron");
+                tgsa.add("chelin");
+                tgsa.add("hyc");
+                tgsa.add("kevin");
+                tgsa.add("jenny");
+                tgsa.add("amy");
+
+                Set<String> torreyPines = new HashSet<String>();
+                torreyPines.add("otto");
+                torreyPines.add("chris");
+                torreyPines.add("hyc");
+
+                Set<String> oms = new HashSet<String>();
+                oms.add("aaron");
+                oms.add("andrew");
+                oms.add("chelin");
+                oms.add("silvia");
+                oms.add("alice");
+                oms.add("sherry");
+                oms.add("jenny");
+
+                Set<String> database = new HashSet<String>();
+                database.add("otto");
+                database.add("andrew");
+                database.add("chelin");
+                database.add("jessica");
+
                 GroupManager manager = ManagerFactory.getGroupManager(getApplicationContext());
+                manager.createGroup(new Group("OMS Club", oms));
+                manager.createGroup(new Group("Torrey Pines Village", torreyPines));
+                manager.createGroup(new Group("TGSA", tgsa));
+                manager.createGroup(new Group("Database Dev", database));
+
+                List<Group> result = manager.getAllGroupsOfUser("otto");
+                for (Group g: result) {
+                    System.out.println(g.getGroupName());
+                }
+                /*
                 mapper.save(new PersonalExpense("1234", 9487, "p1", "none"));
                 mapper.save(new PersonalExpense("2345", 9487.92, "p2", "none"));
                 mapper.save(new PersonalExpense("3456", 9487.123, "p3", "none"));
@@ -51,12 +97,13 @@ public class MainActivity extends AppCompatActivity {
                 mapper.save(new GroupExpense("9876", 1234, "g1", "none"));
                 mapper.save(new GroupExpense("8765", 1234.92, "g2", "none"));
                 mapper.save(new GroupExpense("6543", 1234.123, "g3", "none"));
-
+                */
+                /*
                 DynamoDBScanExpression scanExpression = new DynamoDBScanExpression();
                 PaginatedScanList<PersonalExpense> result1 = mapper.scan(PersonalExpense.class, scanExpression);
                 PaginatedScanList<GroupExpense> result2 = mapper.scan(GroupExpense.class, scanExpression);
-
-                for(PersonalExpense ea: result1){
+                */
+                /*for(PersonalExpense ea: result1){
                     System.out.println(ea.getAmount());
                 }
 
@@ -66,6 +113,7 @@ public class MainActivity extends AppCompatActivity {
                     System.out.println(ea.getAmount());
                 }
 
+                */
                 /*
                 Set<String> res = mapper.load(Group.class,"12341231").getMember();
                 for(String each: res){
