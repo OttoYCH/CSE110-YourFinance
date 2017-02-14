323df267b0ce47f648576c9e8094d36e5fb191b5
Che-Lin Huang <tiesto1114@gmail.com>
2017-02-13 17:35:28 -0800

    group and personal expense

--- app/src/main/java/teamhardcoder/y_fi/database/model/GroupExpenseDAO.java
+++ app/src/main/java/teamhardcoder/y_fi/database/model/GroupExpenseDAO.java
@@ -2,42 +2,28 @@ package teamhardcoder.y_fi.database.model;
 
 import android.content.Context;
 
-import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBScanExpression;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import teamhardcoder.y_fi.database.data.Expense;
-import teamhardcoder.y_fi.database.manager.ExpenseManager;
+import teamhardcoder.y_fi.database.data.GroupExpense;
+import teamhardcoder.y_fi.database.data.PersonalExpense;
+import teamhardcoder.y_fi.database.manager.GroupExpenseManager;
 
 /**
- * Created by otto on 2/13/17.
+ * Created by tiesto1114 on 2/13/17.
  */
 
-public class ExpenseDAO implements ExpenseManager {
+public class GroupExpenseDAO implements GroupExpenseManager {
 
     private DynamoDBMapper db;
 
-    public ExpenseDAO(Context context) {
+    public GroupExpenseDAO(Context context) {
         db = DatabaseHelper.getDBMapper(context);
-    }
-
-    /**
-     * Get all expense of the user
-     * @param userId
-     * @return list of all expense of user
-     */
-    public List<Expense> getUserExpense(String userId) {
-        Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
-        eav.put(":val", new AttributeValue().withS(userId));
-
-        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
-                .withFilterExpression("userId = :val")
-                .withExpressionAttributeValues(eav);
-
-        return db.scan(Expense.class, scanExpression);
     }
 
     /**
@@ -45,7 +31,7 @@ public class ExpenseDAO implements ExpenseManager {
      * @param groupId
      * @return list of all expense of group
      */
-    public List<Expense> getGroupExpense(String groupId) {
+    public List<GroupExpense> getGroupExpense(String groupId) {
         Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
         eav.put(":val", new AttributeValue().withS(groupId));
 
@@ -53,7 +39,7 @@ public class ExpenseDAO implements ExpenseManager {
                 .withFilterExpression("groupId = :val")
                 .withExpressionAttributeValues(eav);
 
-        return db.scan(Expense.class, scanExpression);
+        return db.scan(GroupExpense.class, scanExpression);
     }
 
     /**
@@ -61,8 +47,12 @@ public class ExpenseDAO implements ExpenseManager {
      * @param expenseId
      * @return null if the receipt doesn't exist
      */
-    public Expense getExpense(String expenseId) {
-        return db.load(Expense.class, expenseId);
+    public GroupExpense getExpense(String expenseId) {
+        try {
+            return db.load(GroupExpense.class, expenseId);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     /**
@@ -71,12 +61,12 @@ public class ExpenseDAO implements ExpenseManager {
      * @return true if delete successfully; false otherwise
      */
     public boolean deleteExpense(String expenseId) {
-        Expense e = db.load(Expense.class, expenseId);
-        if (e == null)
-            return false;
-        else {
+        try {
+            GroupExpense e = db.load(GroupExpense.class, expenseId);
             db.delete(e);
             return true;
+        } catch (Exception e)  {
+            return false;
         }
     }
 
@@ -85,9 +75,13 @@ public class ExpenseDAO implements ExpenseManager {
      * @param expense
      * @return true if create successfully; false otherwise
      */
-    public boolean createExpense(Expense expense) {
-        db.save(expense);
-        return true;
+    public boolean createExpense(GroupExpense expense) {
+        try {
+            db.save(expense);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     /**
@@ -95,8 +89,12 @@ public class ExpenseDAO implements ExpenseManager {
      * @param expense
      * @return true if update successfully; false otherwise
      */
-    public boolean editExpense(Expense expense) {
-        db.save(expense);
-        return true;
+    public boolean editExpense(GroupExpense expense) {
+        try {
+            db.save(expense);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
