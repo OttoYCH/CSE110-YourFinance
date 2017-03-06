1a331ce66464dce57ae4f77fa2f04c4eb3959d55
Andrew Chen Hsun Lee <eric781101@gmail.com>
2017-03-05 20:55:56 -0800

    Create GroupBoard

---
+++ app/src/main/java/teamhardcoder/y_fi/GroupBoard.java
@@ -0,0 +1,18 @@
+package teamhardcoder.y_fi;
+
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GroupBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_board);
+
+
+
+    }
+}
